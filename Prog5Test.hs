ba059f24fe9b75a8db5936ccd8b3d876e935dbdd
jneubaum <jacob@neubaum.com>
2019-10-21 21:29:15 -0400

    added program 5 file (no real additions)

---
+++ Prog5Test.hs
@@ -0,0 +1,18 @@
+{-
+"Prog5Test.hs" - Test Cases for HW 5 -> Prog5.hs
+Last Edited: 21 OCT 2019
+West Chester University
+CSC 345 - Programming Language Concepts / Paradigms - Fall 2019
+Original format provided by: Richard Burns, distributed with permission.
+Authors: Mahmoud Gudarzi, Anton Adamovich, Brandon Barker, and Akash Kumar
+
+AUTHORS GIVE NO GUARANTEES THAT TEST CASES ARE CORRECT OR COMPLETE.
+INTRUCTOR HAS FINAL WORD CONCERNING THE FUNCTIONALITY OF YOUR CODE.
+YOU ARE ENCOURAGED TO TEST YOUR CODE INDEPENDENTLY.
+
+Usage: ghci Prog5Test; main
+
+Dependencies: cabal update
+              cabal install tasty
+              cabal install tasty-hunit
+-}
