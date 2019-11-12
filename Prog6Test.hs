5f3cc01d4c551309d4c4b265918aa7148caadf7d
Brandon Barker <contact@brandonbarker.me>
2019-11-12 12:37:57 -0500

    Fix test cases

--- Prog6Test.hs
+++ Prog6Test.hs
@@ -390,7 +390,7 @@ unitTests = testGroup "Unit tests" [
                             Node2 [Leaf2 'c',
                                    Node2 [Leaf2 'd',
                                           Leaf2 'f']]])
-                    3
+                    4
                     countInteriorNodes,
 
             testCase "test_3" $
@@ -403,13 +403,13 @@ unitTests = testGroup "Unit tests" [
                             Node2 [Leaf2 "are",
                                    Node2 [Leaf2 "probably",
                                            Leaf2 "cookies"]]])
-                    4
+                    5
                     countInteriorNodes,
 
             testCase "test_4" $
                 assertTree
                     (Node2 [Node2 [Node2 [Leaf2 10]]])
-                    2
+                    3
                     countInteriorNodes,
 
             testCase "test_5" $
@@ -421,7 +421,7 @@ unitTests = testGroup "Unit tests" [
                                    Leaf2 200004,
                                    Leaf2 200000,
                                    Leaf2 500000]])
-                    1
+                    2
                     countInteriorNodes,
 
             testCase "test_6" $
@@ -429,7 +429,7 @@ unitTests = testGroup "Unit tests" [
                     (Node2 [Leaf2 False,
                             Leaf2 False,
                             Leaf2 False])
-                    0
+                    1
                     countInteriorNodes
         ],
 
