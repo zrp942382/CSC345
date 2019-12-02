87949f95075097ae79fa17d92bc04ce3f8654344
Brandon Barker <contact@brandonbarker.me>
2019-12-02 00:19:20 -0500

    Finish Prog 8 Test Cases

--- Prog8Test.hs
+++ Prog8Test.hs
@@ -17,11 +17,10 @@ Dependencies:
     cabal install tasty-hunit
 -}
 
-import Data.List
-import Prog8
-import System.Environment
-import Test.Tasty
-import Test.Tasty.HUnit
+import           Prog8
+import           System.Environment
+import           Test.Tasty
+import           Test.Tasty.HUnit
 
 main :: IO ()
 main = do
@@ -30,6 +29,10 @@ main = do
 
 tests :: TestTree
 tests = testGroup "Tests" [unitTests]
+
+assertFloat :: (Eq a, Show a, Ord a, Fractional a) =>  a -> a -> Assertion
+assertFloat t v = assertBool msg (abs (t - v) < 1.0e-5)
+    where msg = "Expecting " ++ show v ++ " got " ++ show t ++ " a difference of " ++ show (t - v)
 
 unitTests :: TestTree
 unitTests = testGroup "Unit tests" [
@@ -109,6 +112,85 @@ unitTests = testGroup "Unit tests" [
             testCase "test4" $ assertEqual [] (-216) (product' [1,-2,-3,4,-1,9]),
             testCase "test5" $ assertEqual [] 352000000 (product' [2,11,2,2,2,2,1000,100,10]),
             testCase "test6" $ assertEqual [] 0 (product' [11,-932,0,1,-12,14,5,-81]),
-            testCase "test7" $ assertEqual [] 697546080 (product' [11,-932,1,-12,14,5,81])
+            testCase "test7" $ assertEqual [] 697546080 (product' [11,-932,1,-12,14,5,81]),
+            testCase "test8" $ assertFloat 23.31 (product' [23.31] ),
+            testCase "test9" $ assertFloat 0.0 (product' [18.91, 0.0] ),
+            testCase "test10" $ assertFloat (-246.6414) (product' [15.78, (-15.63)] ),
+            testCase "test11" $ assertFloat (-53.490624) (product' [(-8.17), 0.48, 13.64] ),
+            testCase "test12" $ assertFloat 139157.91828932002 (product' [(-23.41), 16.03, (-20.11), 18.44] ),
+            testCase "test13" $ assertFloat 82352.4731568 (product' [(-16.57), (-15.4), 13.4, 1.8, 13.38] ),
+            testCase "test14" $ assertFloat (-5121.3108390444) (product' [(-1.76), 3.81, 16.45, (-0.49), (-5.65), 16.77] ),
+            testCase "test15" $ assertFloat (-10189.53064429209) (product' [(-5.85), (-2.77), (-3.15), (-16.22), (-6.39), (-2.14), (-0.9)] ),
+            testCase "test16" $ assertFloat (-379769736.48264384) (product' [(-12.57), (-14.69), 13.78, 23.01, (-12.35), (-15.24), 12.67, (-2.72)] ),
+            testCase "test17" $ assertFloat (-1055809.4528509898) (product' [(-19.17), 1.2, 5.66, (-6.08), (-0.77), 7.94, 21.74, 8.88, 1.13] ),
+            testCase "test18" $ assertFloat (-1888.2484863498382) (product' [2.27, (-0.86), 3.07, (-1.37), 3.73, 1.0, (-3.37), (-2.75), (-3.52), 1.89] ),
+            testCase "test19" $ assertFloat 4.801524738200703 (product' [2.4, 4.19, (-4.93), (-2.17), 0.35, 0.56, (-0.5), (-3.78), 0.28, (-3.31), (-0.13)] ),
+            testCase "test20" $ assertFloat 40.14804395272764 (product' [3.64, 3.37, (-0.9), (-2.0), 0.18, 3.34, 2.11, (-1.7), (-0.75), (-0.13), 2.13, (-4.06)] ),
+            testCase "test21" $ assertFloat 20579.073812942548 (product' [4.45, 3.83, (-0.56), 2.13, 2.24, 2.67, 0.52, 3.36, 1.96, 4.89, (-2.86), (-0.93), (-3.8)] ),
+            testCase "test22" $ assertFloat (-6.765471094369319) (product' [(-0.47), (-0.1), (-0.12), 4.0, (-4.2), (-0.51), (-1.03), (-4.04), 4.45, 0.52, 2.45, (-4.71), 2.1, (-0.6)] )
+        ],
+
+        -- 7. max' :: [String] -> [Int]
+        testGroup "test_max'" [
+            testCase "test1" $ assertEqual [] 1 (max' [1]),
+            testCase "test2" $ assertEqual [] 2.0 (max' [1.0, 2.0]),
+            testCase "test3" $ assertEqual [] 5 (max' [3, 5, (-9), 1, 3]),
+            testCase "test4" $ assertEqual [] (-100.0) (max' [(-100.0), (-101.1), (-200.0)]),
+            testCase "test5" $ assertEqual [] (-100) (max' [(-100), (-101), (-200)]),
+            testCase "test6" $ assertEqual [] 'z' (max' ['a', 'b', 'z', 'e']),
+            testCase "test7" $ assertEqual [] "z" (max' ["z", "aa", "abc", "def"])
+        ],
+
+        -- 8. append' :: [a] -> [a] -> [a]
+        testGroup "test_append'" [
+            testCase "test1" $ assertEqual [] ([]::[Int]) (append' [] []),
+            testCase "test2" $ assertEqual [] [1] (append' [] [1]),
+            testCase "test3" $ assertEqual [] [1] (append' [1] []),
+            testCase "test4" $ assertEqual [] [(-2), 3, 4, 5, (-10)] (append' [(-2), 3] [4, 5, (-10)]),
+            testCase "test5" $ assertEqual [] [2,1,4,3,5] (append' [2,1,4] [3,5]),
+            testCase "test6" $ assertEqual [] "pubnub" (append' "pub" "nub"),
+            testCase "test7" $ assertEqual [] "az" (append' "a" "z"),
+            testCase "test8" $ assertEqual [] "az" (append' "" "az"),
+            testCase "test9" $ assertEqual [] [4.0, 1.0, (-2.0), (-3.0)] (append' [] [4.0, 1.0, (-2.0), (-3.0)]),
+            testCase "test10" $ assertEqual [] [4.0, 1.0, (-2.0), (-3.0)] (append' [4.0, 1.0] [(-2.0), (-3.0)]),
+            testCase "test11" $ assertEqual [] "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed" (append' "Lorem ipsum " "dolor sit amet, consectetur adipiscing elit, sed")
+        ],
+
+        -- 9. filterFirst :: (a -> Bool) -> [a] -> [a]
+        testGroup "test_filterFirst" [
+            testCase "test1" $ assertEqual "(true for all elements)" ([]::[Int]) (filterFirst (const True) []),
+            testCase "test2" $ assertEqual "(false for all elements)" ([]::[Int]) (filterFirst (const False) []),
+            testCase "test3" $ assertEqual "(true for all elements)" "" (filterFirst (const True) "a"),
+            testCase "test4" $ assertEqual "(false for all elements)" "a" (filterFirst (const False) "a"),
+            testCase "test5" $ assertEqual "(true for all elements)" "bcd" (filterFirst (const True) "abcd"),
+            testCase "test6" $ assertEqual "(false for all elements)" "abcd" (filterFirst (const False) "abcd"),
+            testCase "test7" $ assertEqual [] [1] (filterFirst (>= 2) [1,4]),
+            testCase "test8" $ assertEqual [] [1,3,4] (filterFirst (\x -> x >= 2) [1,2,3,4]),
+            testCase "test9" $ assertEqual [] [1,3,1] (filterFirst (\x -> x >= 2) [1,2,3,1]),
+            testCase "test10" $ assertEqual [] [2,1] (filterFirst (\x -> x == 3) [3,2,1]),
+            testCase "test11" $ assertEqual [] [2,3] (filterFirst (\x -> x == 1) [2,3,1]),
+            testCase "test12" $ assertEqual [] [1,1] (filterFirst (\x -> x == 1) [1,1,1]),
+            testCase "test13" $ assertEqual [] [5,8] (filterFirst (\x -> x > 0) [10,5,8]),
+            testCase "test14" $ assertEqual [] [3,9,12,8] (filterFirst even [3,10,9,12,8]),
+            testCase "test15" $ assertEqual [] [10,9,12,8] (filterFirst odd [3,10,9,12,8])
+        ],
+
+        -- 10. filterLast :: (a -> Bool) -> [a] -> [a]
+        testGroup "test_filterLast" [
+            testCase "test1" $ assertEqual "(true for all elements)" ([]::[Int]) (filterLast (const True) []),
+            testCase "test2" $ assertEqual "(false for all elements)" ([]::[Int]) (filterLast (const False) []),
+            testCase "test3" $ assertEqual "(true for all elements)" "" (filterLast (const True) "a"),
+            testCase "test4" $ assertEqual "(false for all elements)" "a" (filterLast (const False) "a"),
+            testCase "test5" $ assertEqual "(true for all elements)" "abc" (filterLast (const True) "abcd"),
+            testCase "test6" $ assertEqual "(false for all elements)" "abcd" (filterLast (const False) "abcd"),
+            testCase "test7" $ assertEqual [] [1] (filterLast (\x -> x >= 2) [1,4]),
+            testCase "test8" $ assertEqual [] [1,2,3] (filterLast (\x -> x >= 2) [1,2,3,4]),
+            testCase "test9" $ assertEqual [] [1,2,1] (filterLast (\x -> x >= 2) [1,2,3,1]),
+            testCase "test10" $ assertEqual [] [2,1] (filterLast (\x -> x == 3) [3,2,1]),
+            testCase "test11" $ assertEqual [] [2,3] (filterLast (\x -> x == 1) [2,3,1]),
+            testCase "test12" $ assertEqual [] [1,1] (filterLast (\x -> x == 1) [1,1,1]),
+            testCase "test13" $ assertEqual [] [10,5] (filterLast (\x -> x > 0) [10,5,8]),
+            testCase "test14" $ assertEqual [] [3,10,9,12] (filterLast even [3,10,9,12,8]),
+            testCase "test15" $ assertEqual [] [3,10,12,8] (filterLast odd [3,10,9,12,8])
         ]
     ]
