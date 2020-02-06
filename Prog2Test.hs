24d54e8a4408430d7355c0a62c7ced200c80e34b
Anton Adamovich <antonadamovich@yahoo.com>
2020-02-06 16:35:48 -0500

    Update Prog2Test.hs

--- Prog2Test.hs
+++ Prog2Test.hs
@@ -2,7 +2,7 @@
 "Prog2Test.hs" - Test Cases for HW 2 -> Prog2.hs
 West Chester University - CSC 345 - Programming Language Concepts / Paradigms - Fall 2019
 Original format provided by: Richard Burns , distributed with permission.
-Custom additions for Prog3 by: Mahmoud Gudarzi, Anton Adamovich, and Akash Kumar
+Custom additions for Prog2 by: Mahmoud Gudarzi, Anton Adamovich, and Akash Kumar
 
 AUTHORS GIVE NO GUARANTEES THAT TEST CASES ARE CORRECT OR COMPLETE.
 INTRUCTOR HAS FINAL WORD CONCERNING THE FUNCTIONALITY OF YOUR CODE.
