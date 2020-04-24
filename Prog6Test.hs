3723ee0a299f1a5b3c1234d686f86ef9f73fd5bf
zrp942382 <zperales0480@students.mc3.edu>
2020-04-23 21:06:26 -0400

    Updated with all tests, included a few odds and ends.

--- Prog6Test.hs
+++ Prog6Test.hs
@@ -14,6 +14,17 @@ Dependencies: cabal update
 module Prog6Test where
 import Prog6
 
+-- From: https://smthngsmwhr.wordpress.com/2012/11/09/sorting-algorithms-in-haskell/
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) = quicksort [y | y <- xs, y <= x] ++ [x] ++ quicksort [y | y <- xs, y > x]
+
+setToList :: Set -> [Int]
+setToList (Set x) = [i | i <- x]
+
+maybeSetToList :: Maybe Set -> [Int]
+maybeSetToList (Just (Set x)) = [i | i <- x]
+
 main :: IO ()
 main = do
           putStrLn "Testing Prog6: "
@@ -23,17 +34,47 @@ main = do
                equal EmptySet EmptySet == True && 
                equal (Set []) EmptySet == True &&
                equal EmptySet (Set []) == True &&
-               equal EmptySet (Set [1]) == False &&
+               equal (Set [1]) (Set []) == False &&
+               equal (Set [1]) EmptySet == False &&
                equal (Set []) (Set [1]) == False &&
+               equal EmptySet (Set [1]) == False &&
                equal (Set [1]) (Set [1]) == True &&
                equal (Set [1,2]) (Set [2,1]) == True &&
                equal (Set [1,2,3,4,5]) (Set [4,1,3,2,5]) == True &&
-               equal (Set [1,2,3,4]) (Set [1,2,3,4,5]) == False
+               equal (Set [1,2,3,4]) (Set [1,2,3,4,5]) == False &&
+               equal (Set [1,2,3,4,5]) (Set [1,2,3,4]) == False
+          then putStrLn "Works!"
+          else putStrLn "Does not work."
+
+          putStr "2. saferemove: "  -- 
+          if    maybeSetToList ((saferemove 1 (Set [1]))) == [] && -- Probably could be EmptySet or (Set []). I'd go with making this equal EmptySet in your own code and not (Set []).
+                maybeSetToList ((saferemove 1 (Set [1,2]))) == [2] &&
+                maybeSetToList ((saferemove 1 (Set [2,1]))) == [2] &&
+                quicksort (maybeSetToList ((saferemove 1 (Set [1,3,2,4,5])))) == [2,3,4,5] &&
+                quicksort (maybeSetToList ((saferemove 1 (Set [2,3,1,4,5])))) == [2,3,4,5] &&
+                quicksort (maybeSetToList ((saferemove 1 (Set [2,3,5,4,1])))) == [2,3,4,5] 
+          then putStrLn "Works!"
+          else putStrLn "Does not work."
+
+          putStr "3. union: "
+          if    setToList (union (Set [1]) (Set [1])) == [1] &&
+                quicksort (setToList (union (Set [1,2,3,4]) (Set [3,4,5,6,7]))) == [1,2,3,4,5,6,7] &&
+                quicksort (setToList (union (Set [3,4,5,6,7]) (Set [1,2,3,4]))) == [1,2,3,4,5,6,7] &&
+                quicksort (setToList (union (Set [1,2,3]) (Set [4,5,6]))) == [1,2,3,4,5,6] &&
+                setToList (union (Set []) (Set [1])) == [1] &&
+                setToList (union EmptySet (Set [1])) == [1] &&
+                setToList (union (Set [1]) (Set [])) == [1] &&
+                setToList (union (Set [1]) EmptySet) == [1]
+                -- union EmptySet EmptySet == EmptySet
+                -- union (Set []) EmptySet == EmptySet
+                -- union EmptySet (Set []) == EmptySet
+                -- union (Set []) (Set []) == (Set [])
           then putStrLn "Works!"
           else putStrLn "Does not work."
 
           putStr "4. preorder: " 
-          if    preorder t10 == [0,1,3,4,5,6,7,9,10,11,2] &&
+          if    preorder (Leaf 0) == [0] &&
+                preorder t10 == [0,1,3,4,5,6,7,9,10,11,2] &&
                 preorder t5  == [9,13,16,15,17,19,18,20,8,2,1,3,5,4,6] &&
                 preorder t5' == [9,13,16,15,17,19,18,20,8,2,1,3,22] &&
                 preorder t5'' == [9,13,16,15,17,19,18,20,8] &&
@@ -44,7 +85,8 @@ main = do
           else putStrLn "Does not work."
 
           putStr "5. postorder: " 
-          if    postorder t10 == [3,5,9,10,7,11,6,4,1,2,0] &&
+          if    postorder (Leaf 0) == [0] &&
+                postorder t10 == [3,5,9,10,7,11,6,4,1,2,0] &&
                 postorder t5  == [15,17,16,18,20,19,13,1,3,2,4,6,5,8,9] &&
                 postorder t5' == [15,17,16,18,20,19,13,1,3,2,22,8,9] &&
                 postorder t5'' == [15,17,16,18,20,19,13,8,9] &&
@@ -55,7 +97,9 @@ main = do
           else putStrLn "Does not work."
 
           putStr "6. countZeros: " 
-          if    countZeros t10 == 1 &&
+          if    countZeros (Leaf 0) == 1 &&
+                countZeros tr2' == 2 &&
+                countZeros t10 == 1 &&
                 countZeros t5 == 0 &&
                 countZeros t5' == 0 &&
                 countZeros t5'' == 0 &&
@@ -66,7 +110,9 @@ main = do
           else putStrLn "Does not work."
 
           putStr "7. countLeaves: " 
-          if    countLeaves t10 == 6 &&
+          if    countLeaves (Leaf 0) == 1 &&
+                countLeaves tr2' == 2 &&
+                countLeaves t10 == 6 &&
                 countLeaves t5 == 8 &&
                 countLeaves t5' == 7 &&
                 countLeaves t5'' == 5 &&
@@ -77,7 +123,9 @@ main = do
           else putStrLn "Does not work."
 
           putStr "8. countInteriorNodes: " 
-          if    countInteriorNodes t10 == 5 &&
+          if    countInteriorNodes (Leaf 0) == 0 &&
+                countInteriorNodes tr2' == 1 &&
+                countInteriorNodes t10 == 5 &&
                 countInteriorNodes t5 == 7 &&
                 countInteriorNodes t5' == 6 &&
                 countInteriorNodes t5'' == 4 &&
@@ -88,7 +136,9 @@ main = do
           else putStrLn "Does not work."
 
           putStr "9. depth: " 
-          if    depth t10 == 5 &&
+          if    depth (Leaf 0) == 0 &&
+                depth tr2' == 1 &&
+                depth t10 == 5 &&
                 depth t5 == 3 &&
                 depth t5' == 3 &&
                 depth t5'' == 3 &&
@@ -103,7 +153,8 @@ main = do
           else putStrLn "Does not work."
 
           putStr "10. balanced: " 
-          if    balanced t10 == False &&
+          if    balanced (Leaf 0) == True &&
+                balanced t10 == False &&
                 balanced t5 == True &&
                 balanced t5' == True &&
                 balanced t5'' == False &&
@@ -116,111 +167,3 @@ main = do
                 balanced rr5 == False
           then putStrLn "Works!"
           else putStrLn "Does not work."
-
-{-
-tr1 :: Tree
-tr1 = Node (Leaf 15) 16 (Leaf 17)
-tr2 :: Tree
-tr2 = Node (Leaf 18) 19 (Leaf 20)
-tr2' :: Tree
-tr2' = Node (Leaf 3) 0 (Leaf 0)
-tr2'' :: Tree 
-tr2'' = Node (tr2) 1 (tr2')
-tr2''' :: Tree
-tr2''' = Node (Leaf 33) 0 (tr2'')
-t1 :: Tree
-t1 = Node (Leaf 1) 2 (Leaf 3)
-t2 :: Tree 
-t2 = Node (Leaf 4) 5 (Leaf 6)
-t3 :: Tree
-t3 = Node (t1) 8 (t2)
-t4 :: Tree
-t4 = Node (tr1) 13 (tr2)
-t5 :: Tree 
-t5  = Node (t4) 9 (t3)
-t3' :: Tree
-t3' = Node (t1) 8 (Leaf 22)
-t5' :: Tree
-t5' = Node (t4) 9 (t3')
-t5'' :: Tree
-t5'' = Node (t4) 9 (Leaf 8)
-t6 :: Tree
-t6 = Node (Leaf 9) 7 (Leaf 10)
-t7 :: Tree 
-t7 = Node (t6) 6 (Leaf 11)
-t8 :: Tree
-t8 = Node (Leaf 5) 4 (t7)
-t9 :: Tree 
-t9 = Node (Leaf 3) 1 (t8)
-t10 :: Tree
-t10 = Node (t9) 0 (Leaf 2)
-tree1 :: Tree
-tree1 = Node (Leaf 0) 0 (Leaf 3)
-tree2 :: Tree 
-tree2 = Node (Leaf 0) 5 (tree1)
-tree3 :: Tree
-tree3 = Node (tree2) 0 (Leaf 1)
-tree4 :: Tree
-tree4 = Node (Leaf 0) 0 (tree3)
-w0' :: Tree
-w0' = Node (Leaf 101) 86 (Leaf 102)
-w1 :: Tree
-w1 = Node (Leaf 81) 99 (Leaf 80)
-w2 :: Tree
-w2 = Node (Leaf 83) 98 (Leaf 82)
-w3 :: Tree
-w3 = Node (Leaf 85) 97 (Leaf 84)
-w4 :: Tree
-w4 = Node (Leaf 87) 96 (w0')
-w5 :: Tree
-w5 = Node (Leaf 89) 95 (Leaf 88)
-w6 :: Tree
-w6 = Node (Leaf 91) 94 (Leaf 90)
-w7 :: Tree
-w7 = Node (Leaf 11) 7 (Leaf 12)
-w8 :: Tree
-w8 = Node (Leaf 93) 8 (Leaf 92)
-w9 :: Tree
-w9 = Node (w1) 9 (w2)
-w10 :: Tree
-w10 = Node (w3) 10 (w4)
-w11 :: Tree
-w11 = Node (w5) 5 (w6)
-w12 :: Tree
-w12 = Node (w7) 6 (w8)
-w13 :: Tree
-w13 = Node (w9) 1 (w10)
-w14 :: Tree
-w14 = Node (w11) 2 (w12)
-w15 :: Tree
-w15 = Node (w13) 0 (w14)
-rr :: Tree
-rr = Node (Leaf 14) 12 (Leaf 15)
-rr1 :: Tree
-rr1 = Node (rr) 11 (Leaf 13)
-rr2 :: Tree
-rr2 = Node (Leaf 10) 8 (rr1)
-rr3 :: Tree
-rr3 = Node (Leaf 7) 6 (rr2)
-rr4 :: Tree
-rr4 = Node (rr3) 2 (Leaf 5)
-rr5 :: Tree
-rr5 = Node (Leaf 1) 0 (rr4)
-r1 :: Tree
-r1 = Node (Leaf 13) 9 (Leaf 14)
-r2 :: Tree 
-r2 = Node (Leaf 7) 3 (Leaf 8)
-r3 :: Tree
-r3 = Node (Leaf 11) 6 (Leaf 12)
-r4 :: Tree 
-r4 = Node (r1) 5 (Leaf 10)
-r5 :: Tree
-r5 = Node (r2) 1 (Leaf 4)
-r6 :: Tree
-r6 = Node (r4) 2 (r3)
-r7 :: Tree
-r7 = Node (r5) 0 (r6)
--}
-
-          
-
